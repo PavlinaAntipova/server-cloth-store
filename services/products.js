42b92c5dc79947db38dff2bcebedf589f1fffa94
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-11-01 16:10:36 +0200

    fix get

--- services/products.js
+++ services/products.js
@@ -1,9 +1,8 @@
 const { Product } = require('../models/schemas/product');
 
 const getProductByCondition = async (condition) => {
-  const payload = condition.name.trim();
-  console.log(condition?.["name"]);
   if (condition?.["name"]) {
+    const payload = condition.name.trim();
     return await Product.find({ "name": { $regex: payload, $options: 'i' } });
   }
   return await Product.find(condition);
