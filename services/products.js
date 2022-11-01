ffd4a17d606d11bca1ae7e01f738281f803487bc
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-11-01 21:57:52 +0200

    change find

--- services/products.js
+++ services/products.js
@@ -1,14 +1,18 @@
 const { Product } = require('../models/schemas/product');
 
 const getProductByCondition = async (condition) => {
-
   if (condition?.["name"]) {
     const payload = condition.name.trim();
     return await Product.find({ "name": { $regex: payload, $options: 'i' } });
   }
 
   if (condition?.["parentCategory"] && condition?.["currentCategory"]) {
-    return await Product.find({ category: condition });
+    console.log(condition);
+    if (condition?.s) {
+       return await Product.find({ "category.parentCategory": condition.currentCategory} );
+    } else {
+      return await Product.find({ category: condition });
+    }
   }
 
   return await Product.find(condition);
