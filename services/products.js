c4dafce6cce5e5ca8231a58ece823ed538b45e8f
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-11-01 15:03:26 +0200

    add search option

--- services/products.js
+++ services/products.js
@@ -1,6 +1,10 @@
 const { Product } = require('../models/schemas/product');
 
 const getProductByCondition = async (condition) => {
+  if (condition.title === "name") {
+    return await Product.find(
+    { "name": { "$regex": condition.name, "$options": "i" } }).exec();
+  }
   return await Product.find(condition);
 }
 
