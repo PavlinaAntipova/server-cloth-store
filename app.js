fbfabaed49b7be0e8d9e2c8f5d8a07f41bc5d884
Pavlina Antipova <pavlina.antipova@gmail.com>
2022-11-02 12:49:23 +0200

    cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
 // app.use(cors());
-// app.options('*', cors());
+
 app.use(express.json());
 
 app.use('/api/categories', categoriesRouter);
